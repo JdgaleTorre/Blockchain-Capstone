8e19e2be6b097eff346cb16092b342a437a49291
Jose David Gale <josedavidgale@gmail.com>
2021-09-16 00:07:32 -0600

    feat: Adding contracts and implementing Token mintable and Pausable

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,4 +1,4 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var ERC721MintableComplete = artifacts.require('GaleDotToken');
 
 contract('TestERC721Mintable', accounts => {
 
